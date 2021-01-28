1966968356e2169c46c5ff8d2d26f51356970313
Puneeth159 <60018781+Puneeth159@users.noreply.github.com>
2021-01-28 00:03:53 -0600

    commit

---
+++ init-location.js
@@ -0,0 +1,13 @@
+export default [
+    {
+        Name: 'The Oasis On Lake Travis',
+        Latitude: 30.40586,
+        Longitude: -97.87410
+    },
+    {
+        Name: 'Devotion Tattoo Studio',
+        Latitude: 30.40494, 
+        Longitude: -97.87303
+    }
+
+];
