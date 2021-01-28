03ac8af534b6111b10150a15437b47161735f045
Puneeth159 <60018781+Puneeth159@users.noreply.github.com>
2021-01-28 10:52:45 -0600

    Ran the app locally

--- init-location.js
+++ init-location.js
@@ -1,8 +1,12 @@
 export default [
     {
         Name: 'The Oasis On Lake Travis',
-        Latitude: 30.40586,
-        Longitude: -97.87410
+        Latitude: 38.9633021,
+        Longitude: -95.258007,
+        North:38.9633021,
+        South:38.9633021,
+        East:-95.258007,
+        West:-95.258007
     },
     {
         Name: 'Devotion Tattoo Studio',
